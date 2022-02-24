8ddb74a04689452f97005a35cc691c2689b44ead
hiroki-1219 <hiro236_prog-pc@outlook.jp>
2022-02-24 16:30:45 +0900

    feat: 試しにパーセプトロンでANDを作った - よき

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 compile_main:
-	g++ -Wall -o out main.cpp ./library/LinearAlgebra/* ./library/Plot/* -lm
+	g++ -Wall -o out main.cpp ./library/LinearAlgebra/* ./library/Plot/* ./library/DeepLearning/* -lm
+compile_perceptron:
+	g++ -Wall -o out perceptron.cpp ./library/LinearAlgebra/* ./library/Plot/* ./library/DeepLearning/* -lm
--- perceptron.cpp
+++ perceptron.cpp
@@ -4,6 +4,23 @@
 #include "./header/LinearAlgebra.hpp"
 #include "./header/Plot.hpp"
 
+int AND(int x1, int x2);
+
 int main(void){
-    
+   std::vector<float> check(4);
+   check[0] = AND(0, 0); // 0 0 : 0
+   check[1] = AND(0, 1); // 0 1 : 0
+   check[2] = AND(1, 0); // 1 0 : 0
+   check[3] = AND(1, 1); // 1 1 : 0
+   LinearAlgebra::Vector check_vec(check);
+   check_vec.show();
+}
+
+int AND(int x1, int x2){
+    LinearAlgebra::Vector x({(float)x1, (float)x2});
+    LinearAlgebra::Vector w({0.5, 0.5});
+    float b = -0.7;
+    float tmp = (w * x).sum() + b;
+    if(tmp <= 0) return 0;
+    else return 1;
 }
