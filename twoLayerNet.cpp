4cb7fced13db3ad2422ad6289109945016221a2c
hiroki-1219 <hiro236_prog-pc@outlook.jp>
2022-02-27 01:33:31 +0900

    feat: TwoLayerNetの実装 - accuracyをどうするか考えてる - MNISTのデータセットの読み込み作れ

---
+++ twoLayerNet.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<string>
+#include "./header/LinearAlgebra.hpp"
+#include "./header/DeepLearning.hpp"
+#include "./header/Plot.hpp"
+using namespace DeepLearning;
+
+class TwoLayerNet : public BaseNetwork{
+public:
+    std::map<std::string, LinearAlgebra::Matrix> params;
+    TwoLayerNet(int input_size, int hidden_size, int output_size, float weight_init_std=0.01){
+        this->params["W1"] = weight_init_std * LinearAlgebra::array_randn({input_size, hidden_size});
+        this->params["B1"] = LinearAlgebra::zeros(hidden_size).vec_to_matrix(0);
+        this->params["W2"] = weight_init_std * LinearAlgebra::array_randn({hidden_size, output_size});
+        this->params["B2"] = LinearAlgebra::zeros(output_size).vec_to_matrix(0);
+    }
+    LinearAlgebra::Matrix predict(LinearAlgebra::Matrix x){
+        LinearAlgebra::Matrix W1, W2, B1, B2;
+        W1 = this->params["W1"]; W2 = this->params["W2"];
+        B1 = this->params["B1"]; B2 = this->params["B2"];
+        LinearAlgebra::Matrix a1 = x.dot(W1) + B1;
+        LinearAlgebra::Matrix z1 = Functions::Activation::sigmoid(a1);
+        LinearAlgebra::Matrix a2 = z1.dot(W2) + B2;
+        LinearAlgebra::Matrix y = Functions::Activation::softmax(a2);
+        return y;
+    }
+    float loss(LinearAlgebra::Matrix x, LinearAlgebra::Matrix t){
+        LinearAlgebra::Matrix y = this->predict(x);
+        return Functions::Loss::cross_entropy_loss(y, t);
+    }
+    float accuracy(LinearAlgebra::Matrix x, LinearAlgebra::Matrix t){
+       
+    }
+    std::map<std::string, LinearAlgebra::Matrix> numerical_gradient(LinearAlgebra::Matrix x, LinearAlgebra::Matrix t){
+        auto loss_W = [&x, &t, this](LinearAlgebra::Matrix* W){return this->loss(x, t);};
+        std::map<std::string, LinearAlgebra::Matrix> grads;
+        grads["W1"] = Differential::numerical_gradient(loss_W, &params["W1"]);
+        grads["B1"] = Differential::numerical_gradient(loss_W, &params["B1"]);
+        grads["W2"] = Differential::numerical_gradient(loss_W, &params["W2"]);
+        grads["B2"] = Differential::numerical_gradient(loss_W, &params["B2"]);
+        return grads;
+    }
+};
+
+int main(void){
+
+};
+
